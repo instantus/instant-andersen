95398ba8c04ded6658b28df58fcec736793e8f54
instantus <instanterful@gmail.com>
2020-11-25 11:08:06 +0200

    added user registration using API

---
+++ instant_app/app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller as Controller;
+use App\Http\Requests\StoreApiRequest as StoreApiRequest;
+use App\Models\User as User;
+
+
+class UserController extends Controller
+{
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $validator->errors()->add('field', 'Something is wrong with this field!');
+        });
+    }
+
+    public function store (StoreApiRequest $request) {
+        $user = new User();
+        $user->fill([
+            "email" => $request->email,
+            "password" => bcrypt($request->password)
+        ]);
+        $user->save();
+        if ($user) {
+            $token = $user->createToken('AuthToken')->accessToken;
+            return response(["token" => $token], 201);
+        }
+    }
+}
