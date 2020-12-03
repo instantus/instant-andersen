282c05a196bf2e4cd6b6ec3b20cf6a73dda3f43c
instantus <instanterful@gmail.com>
2020-12-03 15:01:46 +0200

    Small fixes

--- instant_app/tests/Feature/GetUserTest.php
+++ instant_app/tests/Feature/GetUserTest.php
@@ -20,13 +20,12 @@ class GetUserTest extends TestCase
 
     public function testShow()
     {
-        User::factory()->count(2)->create();
-        $user = User::factory()->make();
-        Passport::actingAs($user);
+        $users = User::factory()->count(2)->create();
+        Passport::actingAs($users->last());
 
-        $this->json('GET', 'api/users/1')
+        $this->json('GET', 'api/users/'.$users->first()->id)
             ->assertStatus(403);
-        $this->json('GET', 'api/users/'.$user->id)
+        $this->json('GET', 'api/users/'.$users->last()->id)
             ->assertStatus(200);
     }
 }
